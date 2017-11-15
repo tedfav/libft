91cffd516eadf939760046aa65eff240d55e4d67
Teddy FAVART <tfavart@e1r5p13.42.fr>
2017-11-15 19:53:43 +0100

    modif strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,49 +6,39 @@
 /*   By: tfavart <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 08:44:25 by tfavart           #+#    #+#             */
-/*   Updated: 2017/11/15 19:12:13 by tfavart          ###   ########.fr       */
+/*   Updated: 2017/11/15 19:49:20 by tfavart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static int		ft_len_w(char *str, char c)
+static int		ft_len_w(const char *str, char c)
 {
 	int			len;
-	int			i;
 
 	len = 0;
-	i = 0;
-	while (str[i] == c)
-		i++;
-	while (str[i] != c)
+	while (str[len] != '\0' && str[len] != c)
 	{
 		len++;
-		i++;
 	}
 	return (len);
 }
 
-static int		ft_len_tab(char *str, char c)
+static int		ft_len_tab(const char *str, char c)
 {
 	int			len;
 	int			i;
-	int			first;
 
 	i = 0;
 	len = 0;
-	first = 0;
 	while (str[i] != '\0')
 	{
-		if (str[0] != c)
-			first = 1;
-		if (str[i] != c)
-			if (str[i - 1] == c)
-				len++;
+		if ((i == 0 && str[i] != c) || (str[i] != c && str[i - 1] == c))
+			len++;
 		i++;
 	}
-	return (len + first);
+	return (len);
 }
 
 static void		*ft_free_tab(char **tab)
@@ -74,17 +64,17 @@ char			**ft_strsplit(char const *s, char c)
 	int			len;
 
 	i = 0;
-	len = (s) ? ft_len_tab((char*)s, c) : 0;
-	if (!(tab = (char**)malloc(sizeof(*tab) * (len + 1))))
+	len = (s) ? ft_len_tab(s, c) : 0;
+	if (!(tab = (char**)malloc(sizeof(char *) * (len + 1))))
 		return (NULL);
 	while (i < len)
 	{
 		y = 0;
-		if (!(w = (char*)malloc(sizeof(*w) * (ft_len_w((char*)s, c) + 1))))
-			return (ft_free_tab(tab));
-		while (*s == c)
+		while (*s && *s == c)
 			s++;
-		while (*s != c)
+		if (!(w = (char*)malloc(sizeof(char) * (ft_len_w(s, c) + 1))))
+			return (ft_free_tab(tab));
+		while (*s && *s != c)
 			w[y++] = *(s++);
 		w[y] = '\0';
 		tab[i++] = w;
